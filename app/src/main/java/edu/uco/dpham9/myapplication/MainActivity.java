64e2f1df2b2952a6c23dfc0accc461cc5889f508
DatPham123 <d.pham2013@yahoo.com>
2019-03-26 14:21:43 -0500

    final IoT android app

--- app/src/main/java/edu/uco/dpham9/myapplication/MainActivity.java
+++ app/src/main/java/edu/uco/dpham9/myapplication/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ContextMenu;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -17,28 +21,66 @@ import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button connectBtn;
+    Button playBtn;
+    Button stopBtn;
+    Button upBtn;
+    Button downBtn;
+    Button feedBtn;
     EditText textIP;
-    Socket myAppSocket = null;
+    private WebView webView;
     public static String wifiModuleIp ="";
     public static int wifiModulePort = 0;
     public static String CMD = "0";
+
+
+    //String vidConnection = "http://192.168.1.12:3000";
+    //String vidConnection = "http://techslides.com/demos/sample-videos/small.webm";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        connectBtn = (Button) findViewById(R.id.id_connectBtn);
+        playBtn = (Button) findViewById(R.id.id_play);
+        stopBtn = (Button) findViewById(R.id.id_stop);
+        feedBtn = (Button) findViewById(R.id.id_btn_feed);
         textIP = (EditText) findViewById(R.id.id_ip);
+        final MediaController mediacontroller = new MediaController(this);
+        webView = (WebView) findViewById(R.id.id_web_view);
+        webView.setWebViewClient(new WebViewClient());
 
-        connectBtn.setOnClickListener(new View.OnClickListener() {
+        playBtn.setEnabled(true);
+        stopBtn.setEnabled(false);
+        webView.setVisibility(View.GONE);
+
+        feedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getIPandPort();
-                CMD="Stream";
-                Socket_Async cmdStream = new Socket_Async();
-                cmdStream.execute();
+                CMD="F";
+                Socket_Async cmd_feed_servo = new Socket_Async();
+                cmd_feed_servo.execute();
+            }
+        });
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl("http://192.168.1.14:8000/stream.mp4");
+                webView.setVisibility(View.VISIBLE);
+                playBtn.setEnabled(false);
+                stopBtn.setEnabled(true);
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.stopLoading();
+                webView.setVisibility(View.GONE);
+                playBtn.setEnabled(true);
+                stopBtn.setEnabled(false);
             }
         });
 
@@ -58,14 +100,16 @@ public class MainActivity extends AppCompatActivity {
     public class Socket_Async extends AsyncTask<Void, Void, Void>{
 
         Socket socket;
+
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected Void doInBackground(Void... params) {
             try{
                 InetAddress inetAddress = InetAddress.getByName(MainActivity.wifiModuleIp);
                 socket = new java.net.Socket(inetAddress,MainActivity.wifiModulePort);
                 DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
                 dataOutputStream.writeBytes(CMD);
                 dataOutputStream.close();
+                //socket.close();
             }catch (UnknownHostException e){
                 e.printStackTrace();
             }catch (IOException e){
